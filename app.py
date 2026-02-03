3ab24dfb4e24cdd9cc29fcb7dfc47e2b617612de
Brunaveloso7 <brunavelosoribeiro7@gmail.com>
2026-02-03 16:48:35 -0300

    style: modificação do código

--- app.py
+++ app.py
@@ -12,17 +12,26 @@ def agenda():
     tarefa = None
 
     if request.method == 'POST':
-        titulo_tarefa = request.form.get['titulo_tarefa']
-        data_conclusao = request.form.get['data_conclusao']
+        titulo_tarefa = request.form.get('titulo_tarefa')
+        data_conclusao = request.form.get('data_conclusao')
         tarefa = Tarefa(titulo_tarefa, data_conclusao)
         tarefa.salvar_tarefa()
-        
+
+    tarefas = Tarefa.obter_tarefas()
     return render_template(
         'agenda.html',
         titulo='Agenda',
         tarefa=tarefa
     )
 
+@app.route('delete/<int:idTarefa>')
+def delete(idTarefa):
+    tarefa = Tarefa.id(idTarefa)
+    tarefa.excluir_tarefa()
+
 @app.route('/ola')
 def ola_mundo():
     return "Olá, Mundo!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- models/tarefa.py
+++ models/tarefa.py
@@ -1,13 +1,53 @@
 from models.database import Database
-from typing import Self
+from typing import Self, Any
+from sqlite3 import Cursor
 
 class Tarefa:
-    def __init__(self: Self,titulo_tarefa:str, data_conclusao: str = None) -> None:
+    def __init__(self: Self,titulo_tarefa:str, data_conclusao: str = None, id_tarefa: 
+    int = None) -> None:
         self.titulo_tarefa: str =  titulo_tarefa
         self.data_conclusao:str = data_conclusao
+        self.id_tarefa: int = id_tarefa
     
+    
+    # Tarefa(titulo_tarefa= "Nova tarefa"
+    # Tarefa(titulo_tarefa="Outra tarefa", data_conclusao="2026-02-03")
+    # Tarefa (id=1)
+     
+    @classmethod
+    def id(cls, id: int):
+        with Database('./data/sqlite3') as db:
+           query: str = 'SELECT titulo_tarefa, data_conclusao FROM tarefas  WHERE id= ?;'
+           params: tuple = (id,) 
+           resultado = db.buscar_tudo(query,params)
+
+           # Desempacotamento de coleção
+           titulo, data = resultado
+        return cls(id_tarefa=id, titulo_tarefa=titulo, data_conclusao=data)
+    
+    # Tarefa('Título da Tarefa')
+    # Tarefa('titulo_tarefa= "Outra tarefa", data_conclusao= "2026-02-03")
+
     def salvar_tarefa(self: Self) -> None:
         with Database('./data/tarefas.sqlite3')as db:
             query: str = "I NSERT INTO tarefas (titulo_tarefa, data_conclusao) VALUES (?, ?);"
             params: tuple = (self.titulo_tarefa, self.data_conclusao)
             db.executar(query, params)
+
+    @staticmethod
+    def obter_tarefas() -> list[Self]:
+        with Database('./data/tarefas.sqlite3') as db:
+            query = 'SELECT titulo-tarefa, data_conclusao FROM tarefas;'
+            resultados: list[Any] = db.buscar_tudo(query)
+            tarefas: list[Self] =[Tarefa(titulo, data) for titulo,data in resultados]
+            return tarefas
+    
+    def excluir_tarefa(self):
+        with Database('./data/tarefas.splite3') as db:
+             query: str = 'DELETE FROM tarefas WHERE id = ?;'
+             params: tuple = (self.id_tarefa,)
+             resultado: Cursor = db.executar(query, params)
+             return resultado
+             # resultado = [[]]
+# for titulo, data in resultados
+# Tarefa(titulo, data)
